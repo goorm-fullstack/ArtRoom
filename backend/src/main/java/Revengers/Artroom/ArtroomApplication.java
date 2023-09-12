0720a62bd7ee5152d4acfa9686dc77e8f38f5999
LEE-Donggyu <ehdrb141@naver.com>
2023-09-13 00:11:03 +0900

    Spring, React 환경 구축

---
+++ backend/src/main/java/Revengers/Artroom/ArtroomApplication.java
@@ -0,0 +1,13 @@
+package Revengers.Artroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtroomApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Revengers/Artroom/ArtroomApplicationTests.java
@@ -0,0 +1,13 @@
+package Revengers.Artroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
